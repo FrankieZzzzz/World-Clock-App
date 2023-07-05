4d98742c0fbb15ea4c6c27c77f8503b19449684e
FrankieZzzz <zhongweini0703@gmail.com>
2023-07-05 15:15:13 -0400

    Add setInterval function for list country

--- src/index.js
+++ src/index.js
@@ -18,6 +18,15 @@ function _updateTime(){
     HongKongDateElement.innerHTML = HongKongTimeInfo.format("dddd MMMM/DD/YYYY");
     HongKongTimeElement.innerHTML = HongKongTimeInfo.format("HH:mm:ss [<small>]A[</small>]");
     }
+    //Hongkong info
+    let BuenosAiresElement = document.getElementById("city-BuenosAires");
+    if (BuenosAiresElement){
+    let BuenosAiresDateElement = BuenosAiresElement.querySelector(".date");
+    let BuenosAiresTimeElement = BuenosAiresElement.querySelector(".time");
+    let BuenosAiresTimeInfo = moment().tz("America/Buenos_Aires")
+    BuenosAiresDateElement.innerHTML = BuenosAiresTimeInfo.format("dddd MMMM/DD/YYYY");
+    BuenosAiresTimeElement.innerHTML = BuenosAiresTimeInfo.format("HH:mm:ss [<small>]A[</small>]");
+    }
     //show current time city and date
     let currentTimezone = moment.tz.guess();
     let currentDate = moment().tz(currentTimezone).format("dddd MMMM/DD/YYYY")
@@ -25,28 +34,59 @@ function _updateTime(){
     let topCityAndDateDisplay = document.getElementById("locationAndDate").innerHTML = `${currentTimezone}, ${currentDate}`;
 }
 //refresh evey one sec
-setInterval(_updateTime, 1000);
+// setInterval(_updateTime, 1000);
+let interval = setInterval(_updateTime, 1000)
 
 // List display
-let cityList = document.getElementById("selectCity").addEventListener("change",function _selectFunction(event){
+document
+  .getElementById("selectCity")
+  .addEventListener("change", function _selectFunction(event) {
     let selectCityName = event.target.value;
-    if (selectCityName === "current-Location"){
-        selectCityName = moment.tz.guess();
-    }    
-    let simplifyCityName = selectCityName.replace("_", " ").split("/")[1];
-    let cityTimeZoneName = moment().tz(selectCityName);
+    if (selectCityName === "current-Location") {
+      selectCityName = moment.tz.guess();
+    }
+    listCities.push(selectCityName);
+    //push means add something
     let selectTimeDisplay = document.getElementById("cityListContainer");
-        selectTimeDisplay.innerHTML += 
-        `<div class="cityList">
+    let addedCitiesElement = document.getElementById("addedCities");
+    if (addedCitiesElement) {
+      addedCitiesElement.remove();
+      //remove content
+    }
+    let newCity = document.createElement("div");
+    newCity.id = "addedCities";
+    selectTimeDisplay.appendChild(newCity);
+    updateAddedCities();
+
+    clearInterval(interval);
+    interval = setInterval(() => {
+      _updateTime();
+      updateAddedCities();
+    }, 1000);
+    document.getElementById("cleanBtn").innerHTML = "Clean Cities";
+  });
+
+function updateAddedCities() {
+  let citiesElement = "";
+  listCities.forEach((city) => {
+    let cityName = city.replace("_", " ").split("/")[1];
+    let cityTimeZoneName = moment().tz(city);
+    citiesElement += `<div class="cityList">
             <div class="cityList-left">
-                <p class="cityName">${simplifyCityName}</p>
-                <p class="date">${cityTimeZoneName.format("dddd MMMM/DD/YYYY")}</p>
+                <p class="cityName">${cityName}</p>
+                <p class="date">${cityTimeZoneName.format(
+                  "dddd MMMM/DD/YYYY"
+                )}</p>
             </div>
                 <div class="cityList-right">
-                <span class="time">${cityTimeZoneName.format("HH:mm:ss [<small>]A[</small>]")}</span>
+                <span class="time">${cityTimeZoneName.format(
+                  "HH:mm:ss [<small>]A[</small>]"
+                )}</span>
             </div>
         </div>
-        `
-    let goBack = document.getElementById("cleanBtn").innerHTML = "Clean Cities"
-})
+        `;
+  });
+  document.querySelector("#addedCities").innerHTML = citiesElement;
+}
 
+let listCities = [];
